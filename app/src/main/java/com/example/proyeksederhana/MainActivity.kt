a8a20c01e002b7435bf64f3343ccbf6f7c304fda
JuliaRistyaningrum <juliaristyaningrum123@gmail.com>
2025-05-13 02:04:55 +0700

    Penambahan button toast

--- app/src/main/java/com/example/proyeksederhana/MainActivity.kt
+++ app/src/main/java/com/example/proyeksederhana/MainActivity.kt
@@ -1,47 +1,50 @@
 package com.example.proyeksederhana
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.proyeksederhana.ui.theme.ProyekSederhanaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             ProyekSederhanaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyScreen() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+            Toast.makeText(context, "Selamat datang di Android", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Klik Disini")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewMyScreen() {
     ProyekSederhanaTheme {
-        Greeting("Android")
+        MyScreen()
     }
 }
